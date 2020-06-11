271c099473c52f411e8c4a93a026f102f3284489
lkendre25 <lkendre5652@gmail.com>
2020-06-11 15:35:50 +0530

    Day and Daily wage git status

--- calcWage.sh
+++ calcWage.sh
@@ -7,11 +7,12 @@ read -p "Enter Total Wage " wage
 function getWorkedHours(){
 		totalWage=$@
 		wagePerHour=$[(1*totalWage)/20]
-		
-		#echo $wagePerHour
+		dy=$[wagePerHour/8]
+		echo "Wage Per Day Is "
+		echo $dy
 		Onehr=$[(1*totalWage)/wagePerHour]
-		echo "Wage Per Hours !! "
-		echo $Onehr
+		#echo "Wage Per Hours !! "
+		#echo $Onehr
 		DailyWage=$[Onehr*8]
 		echo "Wage Per Day !! "
 		echo $DailyWage
