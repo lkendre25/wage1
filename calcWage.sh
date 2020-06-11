a9abba215c107b3794df4310ae66fafd521d28bf
lkendre25 <lkendre5652@gmail.com>
2020-06-11 14:11:02 +0530

    Salary counted as per the full and half day using case

--- calcWage.sh
+++ calcWage.sh
@@ -2,11 +2,31 @@
 
 echo "Welcome another wage dir!!"
 
-monthsday=20
-oneDaySalary=160
-for i in {1..20}
-do
-	oneMonthsSalary=$[ $i * oneDaySalary ]
-done
+read -p "Enter worked days either Half day and full day " day
 
-echo $oneMonthsSalary
+perHour=20
+
+
+if [ $(( day )) -gt 4 ]
+then
+	echo " Salary Per Hours"
+
+	case $day in
+	[5-8])
+
+	   for i in {5..8}
+   	do
+      	SalaryForOneDay=$[ $i * $perHour ]
+      	#echo $SalaryForOneDay
+   	done
+			echo $SalaryForOneDay
+   	;;
+
+	*)
+   		echo "60 fix amount"
+
+	esac
+else
+	echo "Loss Of Pay!! "
+fi
+
